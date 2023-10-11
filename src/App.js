91239de49cc1d6615e7c986bcc94eda030916001
pablovns <viniciuspablo962@gmail.com>
2023-10-11 14:45:28 -0300

    Adicionar componentes basicos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { getImageUrl } from "./js/utils.js";
+
+export function Profile({
+  name,
+  imageUrl,
+  profession,
+  nAwards,
+  awards,
+  discoverys,
+}) {
+  return (
+    <section className="profile">
+      <h2>{name}</h2>
+      <img
+        className="avatar"
+        src={getImageUrl(imageUrl)}
+        alt={name}
+        width={70}
+        height={70}
+      />
+      <ul>
+        <li>
+          <b>Profession: </b>
+          {profession}
+        </li>
+        <li>
+          <b>Awards: {nAwards} </b>
+          {awards}
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {discoverys}
+        </li>
+      </ul>
+    </section>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Notable Scientists</h1>
+      <Profile
+        name="Maria Skłodowska-Curie"
+        imageUrl="szV5sdG"
+        profession="physicist and chemist"
+        nAwards="4"
+        awards="(Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)"
+        discoverys="polonium (chemical element)"
+      ></Profile>
+      <Profile
+        name="Katsuko Saruhashi"
+        imageUrl="YfeOqp2"
+        profession="geochemist"
+        nAwards="2"
+        awards="(Miyake Prize for geochemistry, Tanaka Prize)"
+        discoverys="a method for measuring carbon dioxide in seawater"
+      ></Profile>
     </div>
   );
 }
