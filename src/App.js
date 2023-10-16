f80aa3f6f9cd2b2cb378fd90981e13827f8529c9
pablovns <viniciuspablo962@gmail.com>
2023-10-16 08:46:10 -0300

    Mudar tamanho das imagens

--- src/App.js
+++ src/App.js
@@ -1,44 +1,6 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-import { getImageUrl } from "./js/utils.js";
-
-export function Profile({
-  name,
-  imageUrl,
-  imageSize,
-  profession,
-  nAwards,
-  awards,
-  discoverys,
-}) {
-  return (
-    <section className="profile">
-      <div class="flex-row">
-        <img
-          className="avatar"
-          src={getImageUrl(imageUrl)}
-          alt={name}
-          width={100}
-          height={100}
-          />
-        <div className="flex-column-centered-m profile-info">
-          <h2>{name}</h2>
-          <p className="text-muted profile-profession">{profession}</p>
-        </div>
-      </div>
-      <ul>
-        <li>
-          <b>Awards: {nAwards} </b>
-          {awards}
-        </li>
-        <li>
-          <b>Discovered: </b>
-          {discoverys}
-        </li>
-      </ul>
-    </section>
-  );
-}
+import Profile from "./js/components/Profile.js";
 
 function App() {
   return (
@@ -47,6 +9,7 @@ function App() {
       <Profile
         name="Maria Skłodowska-Curie"
         imageUrl="szV5sdG"
+        imageSize={80}
         profession="Physicist and chemist"
         nAwards="4"
         awards="(Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)"
@@ -55,6 +18,7 @@ function App() {
       <Profile
         name="Katsuko Saruhashi"
         imageUrl="YfeOqp2"
+        imageSize={80}
         profession="Geochemist"
         nAwards="2"
         awards="(Miyake Prize for geochemistry, Tanaka Prize)"
