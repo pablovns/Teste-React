14180cb8b409d13566a278aedc60023e1a45d06f
pablovns <viniciuspablo962@gmail.com>
2023-10-16 11:32:59 -0300

    Adicionar componente Card e testar no App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-// import logo from "./logo.svg";
 import "./App.css";
 import Profile from "./js/components/Profile.js";
+import Avatar from "./js/components/Avatar.js";
+import Card from "./js/components/Card.js";
 
 function App() {
   return (
@@ -9,7 +10,6 @@ function App() {
       <Profile
         name="Maria Skłodowska-Curie"
         imageUrl="szV5sdG"
-        imageSize={80}
         profession="Physicist and chemist"
         nAwards="4"
         awards="(Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)"
@@ -18,12 +18,37 @@ function App() {
       <Profile
         name="Katsuko Saruhashi"
         imageUrl="YfeOqp2"
-        imageSize={80}
         profession="Geochemist"
         nAwards="2"
         awards="(Miyake Prize for geochemistry, Tanaka Prize)"
         discoverys="a method for measuring carbon dioxide in seawater."
       ></Profile>
+      <Avatar
+        name="Maria Skłodowska-Curie"
+        imageUrl="szV5sdG"
+        imageSize={90}
+        profession="Physicist and chemist"
+      ></Avatar>
+      <Avatar
+        name="Katsuko Saruhashi"
+        imageUrl="YfeOqp2"
+        imageSize={90}
+        profession="Geochemist"
+      ></Avatar>
+      <Card>
+        <h1>Photo</h1>
+        <img
+          className="avatar"
+          src="https://i.imgur.com/OKS67lhm.jpg"
+          alt="Aklilu Lemma"
+          width={70}
+          height={70}
+        />
+      </Card>
+      <Card>
+       <h1>About</h1>
+       <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
+      </Card>
     </div>
   );
 }
