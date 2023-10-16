b16b9d7991c312a1bacc83b421c14fd45fe6eab8
pablovns <viniciuspablo962@gmail.com>
2023-10-16 08:46:36 -0300

    Separar foto e nome em um componente "Avatar"

---
+++ src/js/components/Avatar.js
@@ -0,0 +1,25 @@
+import { getImageUrl } from "../utils.js";
+
+export default function Avatar({
+  name,
+  imageUrl,
+  imageSize,
+  profession
+}) {
+  return (
+    <div class="flex-row">
+      <img
+        className="avatar"
+        src={getImageUrl(imageUrl)}
+        alt={name}
+        width={imageSize}
+        height={imageSize}
+        />
+      <div className="flex-column-centered-m profile-info">
+        <h2>{name}</h2>
+        <p className="text-muted profile-profession">{profession}</p>
+      </div>
+    </div>
+  );
+  
+}
---
+++ src/js/components/Profile.js
@@ -0,0 +1,32 @@
+import Avatar from "./Avatar.js";
+
+export default function Profile({
+  name,
+  imageUrl,
+  imageSize,
+  profession,
+  nAwards,
+  awards,
+  discoverys
+}) {
+  return (
+    <section className="profile">
+      <Avatar
+        name={name}
+        imageUrl={imageUrl}
+        imageSize={imageSize}
+        profession={profession}
+      ></Avatar>
+      <ul>
+        <li>
+          <b>Awards: {nAwards} </b>
+          {awards}
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {discoverys}
+        </li>
+      </ul>
+    </section>
+  );
+}
