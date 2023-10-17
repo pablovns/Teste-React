0a1a728927be55a091f368b4bc6f9ce6b0073036
pablovns <viniciuspablo962@gmail.com>
2023-10-17 09:33:08 -0300

    Adicionar componente Greeting

---
+++ src/js/components/Greeting.js
@@ -0,0 +1,15 @@
+export default function Greeting() {
+  function handleClick() {
+    const name = prompt('What is your name?');
+    alert(`Hello, ${name}!`);
+  }
+  // A state variable is only necessary to keep information between re-renders of a component. 
+  // Within a single event handler, a regular variable will do fine. 
+  // Don’t introduce state variables when a regular variable works well.
+
+  return (
+    <button onClick={handleClick}>
+      Greet
+    </button>
+  );
+}
